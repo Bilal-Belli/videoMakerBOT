48405785ed38e8a47f002b67d2422d12a729e4da
Bilal <jb_belli@esi.dz>
2023-02-28 15:25:16 +0100

    it work, i add all what i was speaking about, now just need to work on a sweet gui

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
+from PIL import Image
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import numpy as np
 from gtts import gTTS
 from moviepy.editor import *
+import os
 
 # global variables
 cmpt = 1
@@ -19,20 +24,24 @@ def add__widget():
 def create_video2():
     global cmpt
     global entries
+    clips = []
     for entry in entries:
         for j in range(1,cmpt):
             if (entry.winfo_name() == str(j)):
                 text = entry.get("1.0", tk.END)
-                filename = 'output'+str(j)+'.mp3'
+                filename = 'audio'+str(j)+'.mp3'
                 # Generate the audio file
                 tts = gTTS(text=text, lang='fr')
                 tts.save(filename)
     # Load the image
-    img_path = 'image.jpg'
-    img = ImageClip(img_path)
     for j in range(1,cmpt):
+        img_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.jpeg;*.png")])
+        image = Image.open(img_path)
+        resized_image = image.resize((640, 480))
+        img_array = np.array(resized_image)
+        img = ImageClip(img_array)
         # Load the audio
-        audio_path ='output'+str(j)+'.mp3'
+        audio_path ='audio'+str(j)+'.mp3'
         audio = AudioFileClip(audio_path)
         # Set the duration of the video to be the same as the audio
         duration = audio.duration
@@ -41,7 +50,18 @@ def create_video2():
         # Save the video to disk
         video_path = 'video'+str(j)+'.mp4'
         video.write_videofile(video_path, fps=24)
-    
+        clip = VideoFileClip(video_path)
+        clips.append(clip)
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile("video.mp4")
+    # a loop to delete cache audios
+    for j in range(1,cmpt):
+        filename = 'audio'+str(j)+'.mp3'
+        os.remove(filename)
+    # a loop to delete cache videos
+    for j in range(1,cmpt):
+        video_path = 'video'+str(j)+'.mp4'
+        os.remove(video_path)
 
 # Create a Tkinter window
 root = tk.Tk()
